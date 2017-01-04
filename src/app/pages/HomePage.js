fd20070a96bbbb17cd8f17ca7ef06b75d649c484
Suranart Niamcome <suranartnc@gmail.com>
2017-01-04 13:31:04 +0700

    bring back HomePage.js

---
+++ src/app/pages/HomePage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import CSSModules from 'react-css-modules'
+
+import styles from 'styles/pages/Homepage.scss'
+
+const fetchData1 = () => new Promise((resolve, reject) => {
+  setTimeout(() => resolve({
+    data: 'aaa'
+  }), 3000)
+})
+
+const fetchData2 = () => new Promise((resolve, reject) => {
+  setTimeout(() => resolve({
+    data: 'bbb'
+  }), 5000)
+})
+
+@CSSModules(styles)
+class HomePage extends React.Component {
+  static prefetchData = [
+    fetchData1,
+    fetchData2
+  ]
+  render () {
+    return (
+      <div styleName="container">HomePage</div>
+    )
+  }
+}
+
+export default HomePage
