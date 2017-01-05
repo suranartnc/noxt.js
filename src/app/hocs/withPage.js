b57870c30a0e65013cff5ec63f4b4df1f13e9e8e
Suranart Niamcome <suranartnc@gmail.com>
2017-01-05 14:11:54 +0700

    move error reducer inside noxt

--- src/app/hocs/withPage.js
+++ src/app/hocs/withPage.js
@@ -1,6 +1,7 @@
 import React, {Component, PropTypes} from 'react'
 import { connect } from 'react-redux'
-import { resetError } from 'modules/error/errorActions'
+
+import { resetError } from 'noxt/app/redux/modules/error/errorActions'
 
 function withPage () {
   return (ComposeComponent) => {
--- src/app/reducers.js
+++ src/app/reducers.js
@@ -1,7 +1,5 @@
 import postReducer from 'modules/post/postReducer'
-import errorReducer from 'modules/error/errorReducer'
 
 export default {
-  post: postReducer,
-  error: errorReducer
+  post: postReducer
 }
